6363a00907bb94fd9828b03cf1a94139945507fe
phantie <funtom1605@gmail.com>
2023-01-14 13:14:09 +0200

    Minor refactor

--- test/blog_web/controllers/page_controller_test.exs
+++ test/blog_web/controllers/page_controller_test.exs
@@ -3,32 +3,15 @@ defmodule BlogWeb.PageControllerTest do
 
   # generate a test for every "valid" blog post and try to render each
   for id <- Map.keys(Blog.Posts.id_to_valid_post()) do
-    @url "/post/#{id}/"
-    test "GET #{@url}", %{conn: conn} do
-      assert get(conn, @url).status == 200
-    end
-  end
-
-  defp render_posts_pages(conn, page) do
-    status =
-      get(conn, Routes.page_path(conn, :posts) |> BlogWeb.PageView.join_query(%{page: page})).status
-
-    case status do
-      200 -> render_posts_pages(conn, page + 1)
-      404 -> nil
-      _ -> raise "error on page #{page}"
+    @id id
+    test "render post #{@id}", %{conn: conn} do
+      page = render_post(conn, @id)
+      assert page.status == 200
     end
   end
 
   test "GET post list pages", %{conn: conn} do
     render_posts_pages(conn, 1)
-  end
-
-  # render every post to register links that they have for testing
-  defp render_every_post(conn) do
-    for id <- Map.keys(Blog.Posts.id_to_valid_post()) do
-      get(conn, "/post/#{id}/")
-    end
   end
 
   test "ping local links", %{conn: conn} do
@@ -46,5 +29,27 @@ defmodule BlogWeb.PageControllerTest do
     IO.puts("tested links: #{Enum.count(links)}")
 
     assert Enum.empty?(broken_links), "broken links: \n#{inspect(broken_links, pretty: true)}"
+  end
+
+  defp render_posts_pages(conn, page) do
+    status =
+      get(conn, Routes.page_path(conn, :posts) |> BlogWeb.PageView.join_query(%{page: page})).status
+
+    case status do
+      200 -> render_posts_pages(conn, page + 1)
+      404 -> nil
+      _ -> raise "error on page #{page}"
+    end
+  end
+
+  defp render_post(conn, id) do
+    get(conn, "/post/#{id}/")
+  end
+
+  # render every post to register links that they have for testing
+  defp render_every_post(conn) do
+    Blog.Posts.id_to_valid_post()
+    |> Map.keys()
+    |> Enum.map(fn id -> render_post(conn, id) end)
   end
 end
