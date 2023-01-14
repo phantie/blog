fad8d4c4d97a24069a38f139bec7badae74312c4
phantie <funtom1605@gmail.com>
2023-01-14 11:42:08 +0200

    Initial link testing

--- test/blog_web/controllers/page_controller_test.exs
+++ test/blog_web/controllers/page_controller_test.exs
@@ -23,4 +23,28 @@ defmodule BlogWeb.PageControllerTest do
   test "GET post list pages", %{conn: conn} do
     render_posts_pages(conn, 1)
   end
+
+  # render every post to register links that they have for testing
+  defp render_every_post(conn) do
+    for id <- Map.keys(Blog.Posts.id_to_valid_post()) do
+      get(conn, "/post/#{id}/")
+    end
+  end
+
+  test "ping local links", %{conn: conn} do
+    Blog.Test.Links.clear()
+
+    render_every_post(conn)
+
+    links = Blog.Test.Links.local_urls()
+
+    broken_links =
+      links
+      |> Enum.map(fn url -> {url, head(conn, url).status} end)
+      |> Enum.filter(fn {_url, status} -> status != 200 end)
+
+    IO.puts("tested links: #{Enum.count(links)}")
+
+    assert Enum.empty?(broken_links), "broken links: \n#{inspect(broken_links, pretty: true)}"
+  end
 end
